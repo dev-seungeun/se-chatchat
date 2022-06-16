1b7a4ab4ffdb05b6f33c9e6c7ef3d228559e0f6b
백승은 <dev.seungeun@gmail.com>
2022-06-16 14:37:53 +0900

    ChatItem 정리

--- src/components/ChatItem.js
+++ src/components/ChatItem.js
@@ -2,31 +2,42 @@ import React from "react";
 import { _authGetCurrentUser } from "../helpers/auth";
 import { _commonTimestampToDate } from "../helpers/common";
 
+function getChateItem(props) {
+  if(props.theme == "dark") {
+    return <li>
+             <div className = "sender"><span>{'C:\\Users\\'+props.chat.email}</span></div>
+             <div className = "time"><span>{_commonTimestampToDate(props.chat.timestamp)}</span></div>
+             <div className = "message">
+                { props.chat.message.includes("image_send_check")
+                      ? <span className="msg_img" id={'img_id_'+props.chat.message.split(":")[1]} onClick={props.openImageModal}><span className="msg_img_span">{props.chat.imgUrl}</span>#사진#<br/></span>
+                      : props.chat.message.includes("\n")
+                            ? props.chat.message.split("\n").map((line, index) => {
+                                return <span key={index}>{line}<br/></span>
+                              })
+                            : <span>{props.chat.message}<br/></span>
+                }
+             </div>
+           </li>
+  }else {
+    return <li className={props.chat.uid === _authGetCurrentUser().uid ? 'right' : 'left'}>
+             <div className = "time"><span>{_commonTimestampToDate(props.chat.timestamp)}</span></div>
+             <div className = "sender"><span>{props.chat.email}</span></div>
+             <div className = "message">
+                { props.chat.message.includes("image_send_check")
+                      ? <img className="msg_img" id={'img_id_'+props.chat.message.split(":")[1]} onClick={props.openImageModal} src={props.chat.imgUrl} />
+                      : props.chat.message.includes("\n")
+                            ? props.chat.message.split("\n").map((line, index) => {
+                                return <span key={index}>{line}<br/></span>
+                              })
+                            : <span>{props.chat.message}<br/></span>
+                }
+             </div>
+           </li>
+  }
+}
 class ChatItem extends React.Component {
     render() {
-        return(
-            <li className={this.props.chat.uid === _authGetCurrentUser().uid ? 'right' : 'left'}>
-              {
-                this.props.theme == "dark" ? <div className = "sender"><span>{'C:\\Users\\'+this.props.chat.email}</span></div>
-                                           : <div className = "time"><span>{_commonTimestampToDate(this.props.chat.timestamp)}</span></div>
-              }
-              {
-                this.props.theme == "dark" ? <div className = "time"><span>{_commonTimestampToDate(this.props.chat.timestamp)}</span></div>
-                                           : <div className = "sender"><span>{this.props.chat.email}</span></div>
-              }
-              <div className = "message">
-              { this.props.chat.message.includes("image_send_check")
-                ? ( this.props.theme == "dark" ? <span className="msg_img" id={'img_id_'+this.props.chat.message.split(":")[1]} onClick={this.props.openImageModal}><span className="msg_img_span">{this.props.chat.imgUrl}</span>#사진#<br/></span>
-                                               : <img className="msg_img" id={'img_id_'+this.props.chat.message.split(":")[1]} onClick={this.props.openImageModal} src={this.props.chat.imgUrl} /> )
-                : ( this.props.chat.message.includes("\n")
-                       ? this.props.chat.message.split("\n").map((line, index) => {
-                           return <span key={index}>{line}<br/></span>
-                         })
-                       : <span>{this.props.chat.message}<br/></span> )
-              }
-                </div>
-            </li>
-        );
+        return getChateItem(this.props);
     }
 }
 
